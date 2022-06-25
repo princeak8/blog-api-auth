ec1bb4464ec9079fada7ecddde1cfe5bc9f043a3
princeak <akalodave@gmail.com>
2022-06-25 15:30:58 +0100

    latest changes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,7 +16,6 @@ class UserResource extends JsonResource
     {
         return [
                 'id' => $this->id,
-                'name' => $this->name,
                 'domain' => $this->domain,
                 'email' => $this->email,
         ];
