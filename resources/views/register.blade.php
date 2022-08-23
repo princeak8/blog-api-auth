6b9f3a6459166e5683ac82036cddf8e9c64460f8
princeak <akalodave@gmail.com>
2022-08-23 19:17:44 +0100

    chnge

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -19,18 +19,21 @@
                           </div>
                           {!! Form::open(["url"=>"register_user", "method"=>"post", "class"=>"user"]) !!}
                             @include('inc.message')
-                              <div class="form-group">
-                                  <input type="text" name="domain" class="form-control form-control-user" id="exampleDomain" placeholder="Account Domain" value="{{old('domain')}}">
-                              </div>
-                              <div class="form-group">
-                                  <input type="text" name="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email" value="{{old('email')}}">
-                              </div>
-                              <div class="form-group">
-                                  <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                              </div>
-                              <div class="form-group">
-                                  <input type="submit" value="Register"  class="btn btn-primary btn-user btn-block">
-                              </div>
+                                <div class="form-group">
+                                    <input type="text" name="domain_name" class="form-control form-control-user" id="exampleDomain" placeholder="Account Domain name" value="{{old('domain_name')}}">
+                                </div>                             
+                                <div class="form-group">
+                                      <input type="text" name="domain" class="form-control form-control-user" id="exampleDomain" placeholder="Account Domain" value="{{old('domain')}}">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email" value="{{old('email')}}">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" value="Register"  class="btn btn-primary btn-user btn-block">
+                                </div>
                         
                           {{ Form::close() }}
                       </div>
