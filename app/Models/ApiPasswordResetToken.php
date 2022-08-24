4555a41e715318f72808bc670ad3286905158bda
princeak <akalodave@gmail.com>
2022-08-24 17:12:08 +0100

    latest changes

--- app/Models/ApiPasswordResetToken.php
+++ app/Models/ApiPasswordResetToken.php
@@ -9,6 +9,7 @@ class ApiPasswordResetToken extends Model
 {
     use HasFactory;
 
+    protected $table = "api_password_reset_token";
     public static $PASSWORD_RESET_TOKEN = 10;
     public static $PASSWORD_VERIF_TOKEN = 20;
 }
